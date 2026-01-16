5dc93e601cf8e7e0914179f8cc16b7cdb7b1c2fe
kahol <kahola@gmail.com>
2026-01-16 11:05:10 +0100

    added column

--- models/customer.py
+++ models/customer.py
@@ -2,9 +2,15 @@ from datetime import date
 from typing import Annotated, List, Optional
 from database import db
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from enum import Enum as PyEnum
 
 str_255 = Annotated[str, mapped_column(db.String(255))]
 str_100 = Annotated[str, mapped_column(db.String(100))]
+
+
+class UserRole(PyEnum):
+    USER = "USER"
+    ADMIN = "ADMIN"
 
 
 class Customer(db.Model):
@@ -14,6 +20,8 @@ class Customer(db.Model):
     address: Mapped[str_100]
     city: Mapped[str_100]
     date_of_birth: Mapped[date]
+
+    user_role: Mapped[UserRole] = mapped_column(default=UserRole.USER)
     # Optional fields
     telephone: Mapped[Optional[str]] = mapped_column(db.String(20), nullable=True)
     secondary_address: Mapped[Optional[str_255]] = mapped_column(nullable=True)
--- routes/customer_route.py
+++ routes/customer_route.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, current_app, request
 from database import db
 from models.customer import Customer
 
